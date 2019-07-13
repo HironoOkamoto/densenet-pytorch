8a2d9cf4d1455b152200a3a834b726c7ddad0c05
HironoOkamoto <kogepan102@yahoo.co.jp>
2019-07-13 20:54:18 +0900

    update

--- create_weight.sh
+++ create_weight.sh
@@ -1,3 +1,3 @@
-python train.py --dataset svhn --layers 40 --growth 12 --name DenseNet_BC_40_12_50_svhn --epoch 100 --z_dim 50 --no-augment
-python train.py --dataset cifar10 --layers 40 --growth 12 --name DenseNet_BC_40_12_50_cifar10 --epoch 200 --z_dim 50
-python train.py --dataset cifar100 --layers 40  --growth 12 --name DenseNet_BC_40_12_200_cifar100 --num_class 100 --z_dim 200 --epoch 200
+python train.py --dataset svhn --layers 40 --growth 12 --z_dim 50 --name DenseNet_BC_40_12_50_svhn --epoch 100 --no-augment
+python train.py --dataset cifar10 --layers 40 --growth 12 --z_dim 50 --name DenseNet_BC_40_12_50_cifar10 --epoch 200
+python train.py --dataset cifar100 --layers 40  --growth 12 --z_dim 200 --name DenseNet_BC_40_12_200_cifar100 --num_class 100 --epoch 200
